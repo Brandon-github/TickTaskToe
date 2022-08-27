68555e47c440ac5ad284f477b4b80a7e1a0c780a
brandon-github <brandonmachado159@gmail.com>
2022-08-27 14:53:23 -0500

    adicion de seleciona de jugador, turnos y reseteo de valores

---
+++ assets/js/Game.js
@@ -0,0 +1,45 @@
+
+class Game {
+    constructor() {
+        this.buttonGame = document.querySelectorAll('.button--game');
+        this.buttonReset = document.querySelector('.bar__button');
+        this.playerTurn = document.querySelector('.bar__turn--select');
+        this.score = {
+            player1: document.querySelector('.score__point.player1'),
+            player2: document.querySelector('.score__point.player2'),
+            ties: document.querySelector('.score__point.ties')
+        };
+        this.turns = ["x", "o"];
+        this.turn = "x";
+        this.gameOver = false;
+    }
+
+    init() {
+        this.start();
+    }
+
+    start() {
+        if (!this.gameOver) {
+            this.buttonGame.forEach(button => {
+                button.removeAttribute("data-figure");
+    
+                button.addEventListener('click', () => {
+                    button.setAttribute("data-figure", this.turn);
+                    button.setAttribute("disabled", "none");
+                    
+                    if (this.turn === this.turns[0]) this.turn = this.turns[1];
+                    else this.turn = this.turns[0];
+    
+                    this.playerTurn.textContent = this.turn;
+                });
+            });
+        }
+
+
+    }
+
+}
+
+const game = new Game();
+
+game.init();
